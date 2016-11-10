5750fccbbd7d5a19d34e703fe22fd387f0cc6f59
Brad Egler <begler@gmail.com>
2016-11-10 08:50:38 -0500

    local updates

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,18 +1,16 @@
-
-## git user
-#sudo adduser --system --shell /bin/sh --gecos 'git version control' --group --disabled-password --home /home/git git
+#!/bin/sh
 
 ## third party repositories
 #sudo add-apt-repository ppa:otto-kesselgulasch/gimp
 #sudo add-apt-repository ppa:gnome3-team/gnome3-next
 #sudo add-apt-repository ppa:gnome3-team/gnome3-staging
+
 sudo apt-get -yq update
 sudo apt-get -yq dist-upgrade
 
-
 ## everything that needs installed
 sudo apt-get -yq install \
-    guake vim git gitk subversion lib32z1 lib32ncurses5 lib32bz2-1.0 samba system-config-samba \
+    guake vim git gitk subversion lib32z1 lib32ncurses5 samba system-config-samba \
     cifs-utils winbind build-essential checkinstall cdbs devscripts dh-make fakeroot libxml-parser-perl \
     check rar unrar p7zip-rar p7zip zip unzip sharutils uudeview mpack arj cabextract file-roller filezilla \
     filezilla-common gimp gimp-data gimp-data-extras libaio1 openssh-server g++ curl libssl-dev apache2-utils \
@@ -22,33 +20,23 @@ sudo apt-get -yq install \
 
 echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
 
-#wget -O screenfetch 'https://raw.github.com/KittyKatt/screenFetch/master/screenfetch-dev'
+wget -O screenfetch 'https://raw.github.com/KittyKatt/screenFetch/master/screenfetch-dev' && chmod +x screenfetch && sudo mv screenfetch /usr/local/bin
 
-#chmod +x screenfetch
+wget https://bootstrap.pypa.io/ez_setup.py -O - | sudo python && sudo easy_install pip
 
-#sudo mv screenfetch /usr/local/bin
-
-wget https://bootstrap.pypa.io/ez_setup.py -O - | sudo python
-
-sudo easy_install pip
+# wget -qO- https://get.docker.com/ | sh
 
 # Install synergy
 
-wget -q -O - http://archive.getdeb.net/getdeb-archive.key | sudo apt-key add -
-sudo sh -c 'echo "deb http://archive.getdeb.net/ubuntu trusty-getdeb apps" >> /etc/apt/sources.list.d/getdeb.list'
-sudo apt-get -yq update
-sudo apt-get -yq synergy
-
-# Install docker
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
-sudo sh -c "echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
-sudo apt-get -yq update
-sudo apt-get -yq install lxc-docker
+#wget -q -O - http://archive.getdeb.net/getdeb-archive.key | sudo apt-key add -
+#sudo sh -c 'echo "deb http://archive.getdeb.net/ubuntu trusty-getdeb apps" >> /etc/apt/sources.list.d/getdeb.list'
+#sudo apt-get -yq update
+#sudo apt-get -yq synergy
 
 # Add user to docker group so you don't have to sudo anymore
 #sudo groupadd docker
-sudo gpasswd -a begler docker
-sudo service docker restart
+#sudo gpasswd -a begler docker
+#sudo service docker restart
 
 ### Install
 #sudo apt-add-repository ppa:xorg-edgers/ppa
