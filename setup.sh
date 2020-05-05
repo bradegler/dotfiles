1ced2ad6ba00234bcea2d2edbe6e17b87bcaceff
Brad Egler <begler@gmail.com>
2020-05-05 09:33:04 -0400

    fixed setup to auto install oh-my-zsh + theme

--- setup.sh
+++ setup.sh
@@ -40,3 +40,12 @@ do
 done
 
 vim +'PlugInstall --sync' +qa
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+ZSH_CUSTOM=~/.oh-my-zsh/custom
+
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
